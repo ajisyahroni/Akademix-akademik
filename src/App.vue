c611ee07513a2fe1d57f17097f434e3ab71dc437
ajisyahroni <aji.0508@students.amikom.ac.id>
2020-10-22 14:21:58 +0700

    wrap all component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <v-app>
-    
     <v-main>
       <v-container>
         <h1>component creation</h1>
-        <hr>
-        <CardSchedule/>
+        <hr />
+        <CardSchedule />
 
         <h3>Table</h3>
-        <TableSchedule/>
-        
+        <!-- <TableSchedule /> -->
+
+        <h5>Try draggable</h5>
+        <!-- <Draggable/> -->
+        <h3>Card guru</h3>
+        <CardGuru/>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import CardSchedule from './components/Schedule/Card'
-import TableSchedule from './components/Schedule/Table'
+//guru
+import CardGuru from "./components/Guru/Card";
+//schdl
+import CardSchedule from "./components/Schedule/Card";
+import TableSchedule from "./components/Schedule/Table";
+//dbug
+import Draggable from "./components/debug/Dragable";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    CardSchedule,TableSchedule
+    Draggable,
+    CardSchedule,
+    TableSchedule,
+    CardGuru
   },
 
   data: () => ({
